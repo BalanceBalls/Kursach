b59834fd73941757d4bb6f4cc7cc03af2c0cb88a
Alex <alex250mc@gmail.com>
2019-03-10 20:56:57 +0500

    init commit

---
+++ Kursach/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Kursach.Models;
+
+/*Система по ведению учета процесса разработки, включая сроки выполнения,
+ * стоимость услуг, этапов разработки (система многопользовательская)*/
+namespace Kursach.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection => new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult GetUsers()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                var users = conn.Query<User>($@"
+                                        select	id,
+                                                name,
+                                                password
+                                        from Users
+                                       ");
+                return View(users);
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kursach/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace Kursach.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
